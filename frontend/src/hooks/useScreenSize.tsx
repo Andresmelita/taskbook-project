8a4b2d28d411bef6bf325f20f9049b7fb314e1fc
Carlos Andrés Salazar Melita <carlos.salazar.me@usach.cl>
2023-06-28 13:43:22 -0400

    feat: window modified

--- frontend/src/hooks/useScreenSize.tsx
+++ frontend/src/hooks/useScreenSize.tsx
@@ -1,11 +1,12 @@
 import { useState, useEffect } from "react";
 
 const useScreenSize = () => {
-    const [width, setWidth] = useState(window.innerWidth);
-    const [height, setHeight] = useState(window.innerHeight);
+
+    const [width, setWidth] = useState(window?.innerWidth);
+    const [height, setHeight] = useState(window?.innerHeight);
 
     useEffect(() => {
-        window.addEventListener("resize", handleResize);
+        window?.addEventListener("resize", handleResize);
 
         return () => {
             window.removeEventListener("resize", handleResize);
@@ -13,8 +14,8 @@ const useScreenSize = () => {
     }, []);
 
     const handleResize = () => {
-        setWidth(window.innerWidth);
-        setHeight(window.innerHeight);
+        setWidth(window?.innerWidth);
+        setHeight(window?.innerHeight);
     };
 
     return { width, height };
