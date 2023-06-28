0b7e56ef04f594d22dc165a99cd9ed1dbe9a3cc9
Carlos Andrés Salazar Melita <carlos.salazar.me@usach.cl>
2023-06-28 13:06:48 -0400

    feat: navigation modified

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,20 +1,35 @@
 'use client'
-import React from 'react'
+import React, { useState } from 'react'
 import AccountCircleTwoToneIcon from '@mui/icons-material/AccountCircleTwoTone';
 import logo from '../../public/img/logo.png'
 import Image from 'next/legacy/image';
 
 const Navbar = () => {
+
+    const [isLogin, setIsLogin] = useState<Boolean>(false)
+
     return (
-        <div className='navbar flex justify-between items-center px-[30px] fixed h-[56px] w-[100%] bg-[#1F618D]'>
-            <div className='text-[#fff] shadow-text'>
-                <Image src={logo} alt='' width={30} height={30}/>
-            </div>
-            <div className='flex gap-3 shadow-text text-[#fff] items-center'>
-                <p className='text-[16px]'>Crear Cuenta</p>
-                <AccountCircleTwoToneIcon className='text-[34px]' />
-            </div>
-        </div>
+        <div className='navbar px-[30px] fixed h-[56px] w-[100%] bg-[#1c5285] z-[110]'>
+            {isLogin ? < div className='flex justify-between items-center h-[100%] w-[100%]'>
+                <div className='text-[#fff] shadow-text'>
+                    <Image src={logo} alt='' width={30} height={30} />
+                    <p className='family-pacifico'>taskbook</p>
+                </div>
+                <div className='flex gap-3 shadow-text text-[#fff] items-center'>
+                    <p className='text-[16px] cursor-pointer' onClick={() => setIsLogin(false)}>example@email.com</p>
+                    <AccountCircleTwoToneIcon className='text-[34px]' />
+                </div>
+            </div> : <div className='flex justify-between items-center h-[100%] w-[100%]'>
+                <div className='text-[#fff] shadow-text flex gap-2 items-center font-normal'>
+                    <Image src={logo} alt='' width={30} height={30} />
+                    <p className='family-pacifico text-[24px]'>taskbook</p>
+                </div>
+                <div className='flex gap-3 shadow-text text-[#fff] items-center'>
+                    <p className='text-[16px] cursor-pointer' onClick={() => setIsLogin(true)}>Create an Account</p>
+                    <AccountCircleTwoToneIcon className='text-[34px]' />
+                </div>
+            </div>}
+        </div >
     )
 }
 
