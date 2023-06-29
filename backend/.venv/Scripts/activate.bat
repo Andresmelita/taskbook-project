7fafe4efb6a9585d756099b95bd36909cc7c8980
Carlos Andrés Salazar Melita <carlos.salazar.me@usach.cl>
2023-06-29 17:17:59 -0400

    feat: last configuration

--- backend/.venv/Scripts/activate.bat
+++ backend/.venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\salaz\Desktop\backend\.venv
+set VIRTUAL_ENV=C:\Users\salaz\Desktop\Programming\Full Stack\my-taskbook\backend\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
