e2da8d87b4303afdebae900a62ff546d9a2c4401
Carlos Andrés Salazar Melita <carlos.salazar.me@usach.cl>
2023-06-30 11:14:01 -0400

    feat: cors configured

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,22 +1,24 @@
 import click
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 # from flask_migrate import Migrate
 
 app = Flask(__name__)
-cors = CORS(app, resources={r"/users/*": {"origins": "*"}})
+CORS(app)
+# cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/tasks.db'
 db = SQLAlchemy(app)
 # migrate = Migrate(app, db)
 
 @app.route('/')
 def index():
-    return 'Hello World'
+    return {'status': 'Ok'}
 
-@app.route('/users', methods=['GET'])
+@cross_origin
+@app.route('/api/v1/users', methods=['GET'])
 def users():
-    return {'users': [{'userId': 1, 'id': 1, 'title': 'titulo 1', 'body': 'descripción 1'}, {'userId': 2, 'id': 2, 'title': 'titulo 2', 'body': 'descripción 2'}, {'userId': 3, 'id': 3, 'title': 'titulo 3', 'body': 'descripción 3'}]}
+    return {'users': [{'id': 1, 'name': 'Andrés', 'lastName': 'Salazar', 'email': 'carlos.salazar.me@usach.cl', 'password':'Cassette846'}, {'id': 2, 'name': 'Nohelia', 'lastName': 'Rojas', 'email': 'nohelia.rojas2004@gmail.com', 'password':'Prelude9226'}, {'id': 3, 'name': 'Guadalupe', 'lastName': 'Melita', 'email': 'melita.alucema@gmail.com', 'password':'NoteK331'}]}
 
 # @app.cli.command()
 # def createdb():
