df6b0805fb6ab467c6cd0df743e5ea29e7a66218
Carlos Andrés Salazar Melita <carlos.salazar.me@usach.cl>
2023-06-30 13:51:40 -0400

    feat: commits deleted

--- frontend/src/hooks/useScreenSize.tsx
+++ frontend/src/hooks/useScreenSize.tsx
@@ -2,29 +2,6 @@ import { useState, useEffect } from "react";
 
 const useScreenSize = () => {
 
-    // const isClient = typeof window !== "undefined";
-
-    // const [width, setWidth] = useState(isClient ? window.innerWidth : 0);
-    // const [height, setHeight] = useState(isClient ? window.innerHeight : 0);
-
-    // useEffect(() => {
-    //     if (!isClient) {
-    //         return;
-    //     }
-
-    //     const handleResize = () => {
-    //         setWidth(window.innerWidth);
-    //         setHeight(window.innerHeight);
-    //     };
-
-    //     window.addEventListener("resize", handleResize);
-
-    //     return () => {
-    //         window.removeEventListener("resize", handleResize);
-    //     };
-    // }, [isClient]);
-
-    // return { width, height };
     const [width, setWidth] = useState(0);
     const [height, setHeight] = useState(0);
 
