044f12b21f4ce5be39782debbb94ab905dd4cfd1
Carlos Andrés Salazar Melita <carlos.salazar.me@usach.cl>
2023-06-30 03:23:26 -0400

    feat: runserver command created

--- backend/manage.py
+++ backend/manage.py
@@ -8,21 +8,31 @@ cors = CORS(app, resources={r"/users/*": {"origins": "*"}})
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/tasks.db'
 db = SQLAlchemy(app)
 
+
 @app.route('/')
 def home():
     return 'Hello World'
+
 
 @app.route('/users', methods=['GET'])
 def users():
     return {'users': [{'userId': 1, 'id': 1, 'title': 'titulo 1', 'body': 'descripción 1'}, {'userId': 2, 'id': 2, 'title': 'titulo 2', 'body': 'descripción 2'}, {'userId': 3, 'id': 3, 'title': 'titulo 3', 'body': 'descripción 3'}]}
 
-#decorar las funciones con @with_appcontext si requieren acceso al contexto de la aplicación Flask.
-#definir comando personalizado
+# decorar las funciones con @with_appcontext si requieren acceso al contexto de la aplicación Flask.
+# definir comando personalizado
+
+
 @app.cli.command()
 def createdb():
     """Create the database tables."""
     db.create_all()
     click.echo('Database created!')
+
+
+@app.cli.command()
+def runserver():
+    app.run(debug=True)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
