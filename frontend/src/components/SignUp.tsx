205c5372be16f7763e1fda8b42e611f9c3d3f117
Carlos Andrés Salazar Melita <carlos.salazar.me@usach.cl>
2023-07-02 21:30:07 -0400

    feat: sign up configured and operative

--- frontend/src/components/SignUp.tsx
+++ frontend/src/components/SignUp.tsx
@@ -1,9 +1,12 @@
 'use client'
 import useForm from '@/hooks/useForm';
 import Button from '@mui/material/Button';
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
 
 const SignUp = () => {
+
+    const [registrationOk, setRegistrationOk] = useState(false)
 
     const initialValues: any = {
         email: "",
@@ -20,20 +23,45 @@ const SignUp = () => {
     }
 
     const form = useForm({ initialValues });
-    const handleSubmit = (event: any) => {
+
+    const handleSubmit = async (event: any) => {
         event.preventDefault()
-        console.log(form.fields)
+        try {
+            const response = await axios.post('http://127.0.0.1:5000/api/v1/users', {
+                name: form?.fields.name,
+                last_name: form?.fields.last_name,
+                email: form?.fields.email,
+                password: form?.fields.password
+            }, {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+            console.log(response.data);
+            setRegistrationOk(true)
+        } catch (error) {
+            console.error(error);
+            setRegistrationOk(false)
+        }
     }
 
     return (
         <form onSubmit={handleSubmit} className='flex flex-col px-[20px] gap-2'>
-            <input required type='email' {...form.getInput('email')} placeholder={placeholder.email} className='border-[2px] border-[#1F618D] rounded-md p-2'></input>
-            <div className="flex w-[100%] gap-2">
-                <input required type='text' {...form.getInput('name')} placeholder={placeholder.name} className='w-[50%] border-[2px] border-[#1F618D] rounded-md p-2'></input>
-                <input required type='text' {...form.getInput('lastname')} placeholder={placeholder.lastname} className='w-[50%] border-[2px] border-[#1F618D] rounded-md p-2'></input>
-            </div>
-            <input required type='password' {...form.getInput('password')} placeholder={placeholder.password} alt="strongPass" className='border-[2px] border-[#1F618D] rounded-md p-2'></input>
-            <Button type='submit' variant="contained" className='bg-[#1F618D]'>Sign Up</Button>
+            {registrationOk ?
+                <div className='text-[18px] font-semibold flex justify-center items-center'>
+                    <div>Successful registration, Welcome!</div>
+                </div>
+                :
+                <div className='flex flex-col px-[20px] gap-2'>
+                    <input required type='email' {...form.getInput('email')} placeholder={placeholder.email} className='border-[2px] border-[#1F618D] rounded-md p-2'></input>
+                    <div className="flex w-[100%] gap-2">
+                        <input required type='text' {...form.getInput('name')} placeholder={placeholder.name} className='w-[50%] border-[2px] border-[#1F618D] rounded-md p-2'></input>
+                        <input required type='text' {...form.getInput('last_name')} placeholder={placeholder.lastname} className='w-[50%] border-[2px] border-[#1F618D] rounded-md p-2'></input>
+                    </div>
+                    <input required type='password' {...form.getInput('password')} placeholder={placeholder.password} alt="strongPass" className='border-[2px] border-[#1F618D] rounded-md p-2'></input>
+                    <Button type='submit' variant="contained" className='bg-[#1F618D]'>Sign Up</Button>
+                </div>}
+
         </form>
     )
 }
